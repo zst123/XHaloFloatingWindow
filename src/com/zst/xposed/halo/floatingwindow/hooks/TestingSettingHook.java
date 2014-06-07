0edaba626a08b687282a9b61e0ec1e37cb55bfc7
zst123 <manzel.seet@gmail.com>
2014-06-07 13:30:56 +0800

    Revert "Simplify hook for the Testing Area."
    This reverts commit 88af866c5ba748ab53785787e0c1fcd2e9daab71.

--- src/com/zst/xposed/halo/floatingwindow/hooks/TestingSettingHook.java
+++ src/com/zst/xposed/halo/floatingwindow/hooks/TestingSettingHook.java
@@ -2,7 +2,7 @@ package com.zst.xposed.halo.floatingwindow.hooks;
 
 import com.zst.xposed.halo.floatingwindow.Common;
 
-import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -13,8 +13,12 @@ public class TestingSettingHook {
 		if (lpp.packageName.equals(Common.THIS_PACKAGE_NAME)) {
 			Class<?> hookClass = XposedHelpers.findClass("com.zst.xposed.halo.floatingwindow.TestingActivity",
 					lpp.classLoader);
-			XposedBridge.hookAllMethods(hookClass, "initXposedLoaded",
-					XC_MethodReplacement.returnConstant(Boolean.TRUE));
+			XposedBridge.hookAllMethods(hookClass, "initXposedLoaded", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					param.args[0] = true;
+				}
+			});
 		}
 	}
 }
