649b19d98cbae912529ebe09711de52cbd6917c4
zst123 <manzel.seet@gmail.com>
2014-06-08 22:19:07 +0800

    Change Main Preference to a sliding navigation

--- src/com/zst/xposed/halo/floatingwindow/MainPreference.java
+++ src/com/zst/xposed/halo/floatingwindow/MainPreference.java
@@ -1,64 +1,62 @@
 package com.zst.xposed.halo.floatingwindow;
 
-import java.util.ArrayList;
-
 import com.zst.xposed.halo.floatingwindow.preferences.MainFragment;
 import com.zst.xposed.halo.floatingwindow.preferences.MovableFragment;
 
-import android.app.ActionBar;
-import android.app.ActionBar.OnNavigationListener;
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.widget.ArrayAdapter;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
 
-public class MainPreference extends PreferenceActivity implements OnNavigationListener {
+public class MainPreference extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		ArrayList<String> itemList = new ArrayList<String>();
-		itemList.add (getResources().getString(R.string.pref_main_top_title));
-		itemList.add(getResources().getString(R.string.pref_movable_top_title));
-		itemList.add(getResources().getString(R.string.pref_testing_top_title));
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1, itemList);
+		setContentView(R.layout.activity_viewpager);
 		
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-	    actionBar.setListNavigationCallbacks(adapter, this);
-	    
-	    getFragmentManager().beginTransaction().replace(android.R.id.content,
-				MainFragment.getInstance()).addToBackStack(null).commit();
-	}
-
-	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-		getActionBar().setSelectedNavigationItem(itemPosition);
-		switch (itemPosition) {
-		case 0:
-			getFragmentManager().beginTransaction().replace(android.R.id.content,
-					MainFragment.getInstance()).commit();
-			break;
-		case 1:
-			getFragmentManager().beginTransaction().replace(android.R.id.content,
-					MovableFragment.getInstance()).commit();
-			break;
-		case 2:
-			getFragmentManager().beginTransaction().replace(android.R.id.content,
-					TestingActivity.getInstance()).commit();
-			break;
-		}
-		return false;
-	}
-	
-	@Override
-	public void onBackPressed() {
-	    int fragments = getFragmentManager().getBackStackEntryCount();
-	    if (fragments == 1) { 
-	        finish();
-	    }
-	    // http://stackoverflow.com/questions/20340303/
-	    super.onBackPressed();
+		FragmentPagerAdapter adapter = new FragmentPagerAdapter(getFragmentManager()) {
+			@Override
+			public Fragment getItem(int position) {
+				switch (position) {
+				case 0:
+					return MainFragment.getInstance();
+				case 1:
+					return MovableFragment.getInstance();
+				case 2:
+					return TestingActivity.getInstance();
+				}
+				return new Fragment();
+			}
+			
+			@Override
+			public String getPageTitle(int pos) {
+				switch (pos) {
+				case 0:
+					return getResources().getString(R.string.pref_main_top_title);
+				case 1:
+					return getResources().getString(R.string.pref_movable_top_title);
+				case 2:
+					return getResources().getString(R.string.pref_testing_top_title);
+				}
+				return "";
+			}
+			
+			@Override
+			public int getCount() {
+				return 3;
+			}
+		};
+		ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+		viewPager.setAdapter(adapter);
+		
+		PagerTabStrip pts = (PagerTabStrip) findViewById(R.id.pager_title_strip);
+		pts.setTabIndicatorColor(0xFF333333);
+		pts.setTextColor(0xFF333333);
+		pts.setBackgroundColor(Color.TRANSPARENT);
 	}
 }
